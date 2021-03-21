71cbbc98ea72f8440f83c6c7d61d0c11c451f9e2
Owen <owen.allemang@gmail.com>
2021-03-21 02:50:00 +0000

    removed redundant parentheses

--- video_interpreter.py
+++ video_interpreter.py
@@ -16,7 +16,7 @@ def get_video_feed():
 
         # Display the resulting frame
         cv2.imshow('frame', gray)
-        if (cv2.waitKey(1) & 0xFF == ord('q')):
+        if cv2.waitKey(1) & 0xFF == ord('q'):
             break
 
     # When everything done, release the capture
