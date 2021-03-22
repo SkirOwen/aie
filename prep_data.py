2f4e3660a9c21988cdb49e8aad75ef6e3c418587
Owen <owen.allemang@gmail.com>
2021-03-22 17:24:32 +0000

    created prep_data.py

---
+++ prep_data.py
@@ -0,0 +1,4 @@
+def load_coco():
+    # https://github.com/cocodataset/cocoapi/blob/master/PythonAPI/pycocoDemo.ipynb
+    # https://github.com/cocodataset/cocoapi/blob/master/PythonAPI/pycocotools/coco.py
+    pass
