3e08ecdd905d3e40c4be1371e67dbf47ae64949f
Owen <owen.allemang@gmail.com>
2021-03-22 17:34:41 +0000

    simplify syntax of check

--- video_interpreter.py
+++ video_interpreter.py
@@ -31,9 +31,9 @@ def save_video():
     fourcc = cv2.VideoWriter_fourcc(*'XVID')
     out = cv2.VideoWriter('output.avi', fourcc, 20.0, (640, 480))
 
-    while(cap.isOpened()):
+    while cap.isOpened():
         ret, frame = cap.read()
-        if ret==True:
+        if ret:
             frame = cv2.flip(frame, 0)
 
             # write the flipped frame
